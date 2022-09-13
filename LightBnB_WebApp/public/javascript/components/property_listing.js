8a1fcecfad4e687b0e0f7e593737644f4b6f4a3f
ej8899 <ej8899@gmail.com>
2022-09-13 15:08:26 -0600

    add thumbnail image color shift

--- LightBnB_WebApp/public/javascript/components/property_listing.js
+++ LightBnB_WebApp/public/javascript/components/property_listing.js
@@ -42,7 +42,7 @@ $(() => {
     return `
     <article class="property-listing">
         <section class="property-listing__preview-image">
-          <img src="${property.thumbnail_photo_url}" alt="house" id="listingid${property.id}">
+          <img src="${property.thumbnail_photo_url}" alt="house" class="imgthumb" id="listingid${property.id}">
         </section>
         <section class="property-listing__details">
           <h3 class="property-listing__title">${property.title}</h3>
@@ -51,9 +51,9 @@ $(() => {
             <li>number_of_bathrooms: ${property.number_of_bathrooms}</li>
             <li>parking_spaces: ${property.parking_spaces}</li>
           </ul>
-          ${isReservation ? 
-            `<p>booked: ${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>` 
-            : ``}
+          <p>${isReservation ? 
+            `booked: ${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}` 
+            : ``}&nbsp;</p>
           <footer class="property-listing__footer">
             <div class="property-listing__rating tooltip expand" data-title="${toolTipRating}">${finalStars}</div>
             <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
